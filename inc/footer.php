1d7c907a9d41cfd7405c85f5e11694c117fd41d5
MilosIlic7198 <m.iWork71@gmail.com>
2022-07-26 14:20:31 +0200

    Commit nb.1

---
+++ inc/footer.php
@@ -0,0 +1,60 @@
+</div>
+    </main>
+
+    <div class="container">
+      <footer
+        class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top"
+      >
+        <div class="col-md-4 d-flex align-items-center">
+          <a
+            href="#"
+            class="mb-3 me-2 mb-md-0 text-black text-decoration-none lh-1"
+          >
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/7471/7471556.png"
+              alt="logo"
+              width="40"
+              height="40"
+              role="img"
+              aria-label="feedback"
+            />
+          </a>
+          <span class="text-black">Copyright &copy; Miloš Ilić 2022</span>
+        </div>
+
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <li class="ms-3">
+            <a class="text-black" href="#"
+              ><img
+              src="https://cdn-icons-png.flaticon.com/512/2525/2525779.png"
+              alt="logo" class="bi me-2" width=25" height="25" role="img"
+              aria-label="twitter"/></a
+            >
+          </li>
+          <li class="ms-3">
+            <a class="text-black" href="#"
+              ><img
+              src="https://cdn-icons-png.flaticon.com/512/1400/1400487.png"
+              alt="logo" class="bi me-2" width=25" height="25" role="img"
+              aria-label="instagram"/></a
+            >
+          </li>
+          <li class="ms-3">
+            <a class="text-black" href="#"
+              ><img
+              src="https://cdn-icons-png.flaticon.com/512/1400/1400477.png"
+              alt="logo" class="bi me-2" width=25" height="25" role="img"
+              aria-label="facebook"/></a
+            >
+          </li>
+        </ul>
+      </footer>
+    </div>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
